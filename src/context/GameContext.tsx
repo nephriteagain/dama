b3a3a6cd9e440436064ee19f6aab871c00c51c34
nephriteagain <110908855+nephriteagain@users.noreply.github.com>
2023-02-15 15:31:44 +0800

    both sides can now forward

--- src/context/GameContext.tsx
+++ src/context/GameContext.tsx
@@ -10,37 +10,50 @@ export const GlobalProvider = ({children}) => {
   const [ boardData, setBoardData ] = useState(arrayData)
   const [ pieceToMove, setPieceToMove ] = useState(null)
 
-  function highlightMoves(xPosition: number, yPosition: number, piece : (string | null)) {
-    // how to make piece move?
-    // square must be empty
-    // only allow to move forward
-    // only allow at white area
-    // when i clicked it it must highlight its possible move
-    // and when i clicked the highlighted area it will changes its position
-    // when i click anywhere else it should cancel
-    // if there is no available move return
-    // this operation should highlight the piece possible move
-    // need a state for piece that was clicked
+  function highlightMoves(xPosition: number, yPosition: number, piece : (string | null), player : number) {
+
     if (piece === null) return
     const newBoardData = boardData.map((item) => {
       if (!item.playable) return item
+        // p1  right move
         if (
             item.x === xPosition + 1 && 
             item.y === yPosition - 1 &&
-            item.piece === null
+            item.piece === null &&
+            player === 1
           ) {
             return {...item, highlighted: true}
         }
+        // p1 left move
         if (
           item.x === xPosition - 1 &&
           item.y === yPosition - 1 &&
-          item.piece === null
+          item.piece === null &&
+          player === 1
+        ) {
+            return {...item, highlighted: true}
+        }
+        // p2 right move
+        if (
+            item.x === xPosition + 1 && 
+            item.y === yPosition + 1 &&
+            item.piece === null &&
+            player === 2
+          ) {
+            return {...item, highlighted: true}
+        }
+        // p2 left move
+        if (
+          item.x === xPosition - 1 &&
+          item.y === yPosition + 1 &&
+          item.piece === null &&
+          player === 2
         ) {
             return {...item, highlighted: true}
         }
         return {...item, highlighted: false}
       })
-  setPieceToMove({piece, x: xPosition, y: yPosition})
+  setPieceToMove({piece, x: xPosition, y: yPosition, player})
   setBoardData([...newBoardData])
 }
 
@@ -52,21 +65,57 @@ export const GlobalProvider = ({children}) => {
     })
     const newBoardData = boardData.map((item) => {
       if (!item.playable) return item
-      if (item === chipToMove) return {...item, piece: null, player: null}
+
+      if (item === chipToMove) {
+        return {...item, piece: null, player: null}
+      }
+
       if (
         item.x === xPosition &&
         item.y ===  yPosition
-      ) return {...item, piece: pieceToMove.piece, highlighted: false, player: player}
+      ) return {...item, piece: pieceToMove.piece, highlighted: false, player: pieceToMove.player}
+
       return {...item, highlighted: false}
     })
     setPieceToMove(null)
     setBoardData([...newBoardData])
   }
 
+  {/*
+  how to eat a piece?
+  a piece must be on range
+  a piece of opposite type right to each other
+  the landing area is empty
+  after the transaction, the opponent must have fewer piece
+  also account if there is a probable multiple possible piece to take
+  */}
+
+
+
+
+
+
+
+
+
+
+
+
 
   useEffect(() => {
-    console.log(pieceToMove)
+    if (pieceToMove) {
+      console.log(pieceToMove)
+    }
+    //  else {
+    //   console.log(boardData)
+    // }
   }, [boardData])
+
+
+
+
+
+
 
   return (
     <GlobalContext.Provider value={{
